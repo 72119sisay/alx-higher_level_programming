23d99d62b60162b0e17bc847aad29e8a6eb25427
sisay72119 <sisayfikru69@gmail.com>
2023-03-13 12:51:17 +0000

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
 
-const myVar1 = "C is fun";
-const myVar2 = "Python is cool";
-const myVar3 = "JavaScript is amazing";
-console.log(myVar1; myVar2; myVar3);
+const myVar = "C is fun"\n"Python is cool"\n"JavaScript is amazing";
+console.log(myVar);
